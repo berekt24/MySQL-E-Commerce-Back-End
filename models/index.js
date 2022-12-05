c8a5db89875589aa6b8ccb78ef8ec0afb13529cc
berekt24 <bashenafi35@gmail.com>
2022-12-05 18:53:56 -0500

    fixed relationships between tables

--- models/index.js
+++ models/index.js
@@ -12,13 +12,30 @@ Product.belongsTo(Category, {
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, { through: ProductTag });
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+   // Define an alias for when data is retrieved
+   as: 'product_tags'
+  });
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, { through: ProductTag });
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+   // Define an alias for when data is retrieved
+   as: 'tag_products'
+  });
 
 module.exports = {
   Product,
