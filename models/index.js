0287550881a1281955af6d511b76cd0bece7b289
berekt24 <bashenafi35@gmail.com>
2022-12-05 21:27:46 -0500

    fixed get route for tag and fixed relationship

--- models/index.js
+++ models/index.js
@@ -23,7 +23,7 @@ Product.belongsToMany(Tag, {
     unique: false
   },
    // Define an alias for when data is retrieved
-   as: 'product_tags'
+  //  as: 'product_tags',
   });
 
 // Tags belongToMany Products (through ProductTag)
@@ -34,7 +34,7 @@ Tag.belongsToMany(Product, {
     unique: false
   },
    // Define an alias for when data is retrieved
-   as: 'tag_products'
+  //  as: 'tags',
   });
 
 module.exports = {
